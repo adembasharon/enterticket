418c611d0fe47d412acbff87d93231d0f3751a89
adembasharon <adembasharon816@gmail.com>
2022-07-26 13:56:59 +0300

    Updates

---
+++ precache-manifest.286311cbce2e7d3aab2884203ba9312b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cd5fa255da910184971",
+    "url": "/static/css/main.9e9fe445.chunk.css"
+  },
+  {
+    "revision": "6cd5fa255da910184971",
+    "url": "/static/js/main.62e8b89a.chunk.js"
+  },
+  {
+    "revision": "adbb01432f593ec793ed",
+    "url": "/static/js/runtime~main.15762efe.js"
+  },
+  {
+    "revision": "d59be24ad5043533b66f",
+    "url": "/static/js/2.f1649a1f.chunk.js"
+  },
+  {
+    "revision": "32f2623d876e9f38ffc7",
+    "url": "/static/js/3.6a179e71.chunk.js"
+  },
+  {
+    "revision": "2c0dcbb90604c8752aeb",
+    "url": "/static/js/4.0cdc0a4b.chunk.js"
+  },
+  {
+    "revision": "189f30201fa14c1e2ccf",
+    "url": "/static/js/5.30f03adb.chunk.js"
+  },
+  {
+    "revision": "10b665a8652efcd413ec253d0213a95b",
+    "url": "/index.html"
+  }
+];
